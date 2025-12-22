2a4879a6076df22a20c2a5209b5f9ab9574737cf
lokhacodes <sumaiyasonali421@gmail.com>
2025-12-22 19:39:50 +0600

    making links clickable

--- Chrome Ext/index.js
+++ Chrome Ext/index.js
@@ -1,18 +1,30 @@
 
-let myLeads= ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads= []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-console.log(ulEl)
+
+
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+   // ulEl.innerHTML+= "<li>" +inputEl.value + "</li>"
+    inputEl.value = ""
+    renderLeads()
 })
 
 
-    for(let i=0; i< myLeads.length;i++){
-      ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-   // document.createElement("li") }
-   // li.textContent = myLeads[i]
-   // ulEl.ariaExpanded(li)
-    
+
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+      `
+    }
+    ulEl.innerHTML = listItems
+}
