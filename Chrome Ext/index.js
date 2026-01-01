0cdedaa85d6cc120ad57ac09aa491c0a650c787d
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-01 12:56:58 +0600

    new

--- Chrome Ext/index.js
+++ Chrome Ext/index.js
@@ -18,7 +18,7 @@ tabBtn.addEventListener("click",function(){
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
 
         myLeads.push(tabs[0].url)
-        localStorage.setItems("myLeads",JSON.stringify(myLeads))
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
         render(myLeads)
 
     })
